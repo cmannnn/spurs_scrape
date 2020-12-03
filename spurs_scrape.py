c8fc44a5b3fadfaff5f66de72fd5ae6b7d802b2a
Chris Murphy <61854922+cmannnn@users.noreply.github.com>
2020-12-03 17:25:50 -0500

    Update spurs_scrape.py

--- spurs_scrape.py
+++ spurs_scrape.py
@@ -25,17 +25,14 @@ player_clean_sheets = player_goals4.find_all('p')[5].text
 
 # current league table
 current_league_table = pd.read_html('https://fbref.com/en/comps/9/Premier-League-Stats')[0]
+current_league_table = current_league_table.reset_index(drop=True)
 current_league_table = current_league_table.set_index('Rk')
-current_league_table = current_league_table.drop(['Notes', 'Attendance'], axis=1)
+current_league_table = current_league_table.drop(columns=['Notes', 'Attendance'])
 
 #print(current_league_table.info())
 
 # squad stats NOT PULLING SECOND TABLE IN CORRECTLY
 squad_stats_table = pd.read_html('https://fbref.com/en/comps/9/Premier-League-Stats', header=1)[1]
 
-# dropping necessary columns + cleaning column names
-squad_stats_table = squad_stats_table.drop(columns=['MP.1', 'W.1', 'D.1', 'L.1', 'GF.1', 'GA.1', 'GDiff.1', 'Pts.1', 'Pts/G.1', 'xG.1', 'xGA.1', 'xGDiff.1', 'xGDiff/90.1'])
-squad_stats_table = squad_stats_table.rename(columns={'Rk': 'Rank', 'MP': 'Matches Played'})
-
-
-print(squad_stats_table.columns)
+# cleaning column names
+#squad_stats_table = squad_stats_table.rename(columns={'Rk': 'Rank', 'MP': 'Matches Played'})
